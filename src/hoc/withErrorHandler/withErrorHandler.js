bce64016b7474df9d761dc09257679ed7d17a318
er.rajatmalik <er.rajatmalik@gmail.com>
2020-02-18 21:50:18 +0530

    unmount axios

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,16 +8,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     };
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptors = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptors = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error: error });
         }
       );
+    }
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptors);
+      axios.interceptors.response.eject(this.reqInterceptors);
     }
     errorConfirmedHandler = () => {
       this.setState({ error: null });
