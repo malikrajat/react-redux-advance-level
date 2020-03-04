3e7794d3f985354a9ffd8e06047b39edfda207d7
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-04 20:17:17 +0530

    add store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
-const apps = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
-ReactDOM.render(apps, document.getElementById("root"));
+
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
