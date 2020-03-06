dfd7149cf0d25a9f8d458f4e9b6626edce6c09d4
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-06 22:48:45 +0530

    combineReducers

--- src/index.js
+++ src/index.js
@@ -2,27 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import reducer from "./store/reducers/burgerBuilder";
+import thunk from "redux-thunk";
+import orderReducer from "./store/reducers/order";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
-const logger = store => {
-  return next => {
-    return action => {
-      console.log("[Middleware] Dispatching", action);
-      const result = next(action);
-      console.log("[Middleware] next state", store.getState());
-      return result;
-    };
-  };
-};
+// const logger = store => {
+//   return next => {
+//     return action => {
+//       console.log("[Middleware] Dispatching", action);
+//       const result = next(action);
+//       console.log("[Middleware] next state", store.getState());
+//       return result;
+//     };
+//   };
+// };
+
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 const app = (
