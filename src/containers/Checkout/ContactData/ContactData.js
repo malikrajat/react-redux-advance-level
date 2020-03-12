ab8d3207b997184dcbf886e7d6c92b8c0493fc2f
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-12 14:55:59 +0530

    add

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -217,7 +217,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: orderData => dispatch(actions.purchaseBurgerStart(orderData))
+    onOrderBurger: orderData => dispatch(actions.purchaseBurger(orderData))
   };
 };
 export default connect(
