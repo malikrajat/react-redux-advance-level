57326e20760355e5dcac2f42e6d3f2d0ce1df9ff
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-12 14:59:05 +0530

    add auth

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,4 +3,5 @@ export {
   removeIngredient,
   initIngredients
 } from "./burgerBuilder";
-export { purchaseBurgerStart } from "./order";
+export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export { auth } from "../../containers/Auth/Auth";
