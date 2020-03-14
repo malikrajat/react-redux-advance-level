8f7d61b195e5feac5392871eb4e87a5523715158
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-14 13:11:11 +0530

    update auth

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import thunk from "redux-thunk";
 import orderReducer from "./store/reducers/order";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import authReducer from "./store/reducers/auth";
 
 // const logger = store => {
 //   return next => {
@@ -25,7 +26,8 @@ import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
+  order: orderReducer,
+  auth: authReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
